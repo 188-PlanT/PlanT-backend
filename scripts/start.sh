e9bf5a3bcd7e852d6809ef6f3f8bcf3bf3a0749c
kimsh1017 <81338287+kimsh1017@users.noreply.github.com>
2024-03-18 19:59:04 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,11 +9,11 @@ START_LOG="$ROOT_PATH/start.log"
 
 NOW=$(date +%c)
 
-echo "[$NOW] $JAR 복사" >> $START_LOG
+#echo "[$NOW] $JAR 복사" >> $START_LOG
 cp $ROOT_PATH/build/libs/spring-github-action-1.0.0.jar $JAR
 
-echo "[$NOW] > $JAR 실행" >> $START_LOG
+#echo "[$NOW] > $JAR 실행" >> $START_LOG
 nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
 
 SERVICE_PID=$(pgrep -f $JAR)
-echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
+#echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
