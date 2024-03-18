6509f8b6186a26a34f2f7093876e8ccf76f30a4f
kimsh1017 <81338287+kimsh1017@users.noreply.github.com>
2024-03-18 20:12:17 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,10 +10,11 @@ START_LOG="$ROOT_PATH/start.log"
 NOW=$(date +%c)
 
 #echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/build/libs/blazingDevs_calendar-0.0.1-SNAPSHOT.jar $JAR
+#cp $ROOT_PATH/build/libs/blazingDevs_calendar-0.0.1-SNAPSHOT.jar $JAR
 
 #echo "[$NOW] > $JAR 실행" >> $START_LOG
-nohup java -jar $JAR --spring.profiles.active=prod & #> $APP_LOG 2> $ERROR_LOG
+#nohup java -jar $JAR --spring.profiles.active=prod & #> $APP_LOG 2> $ERROR_LOG
+nohup java -jar $ROOT_PATH/build/libs/blazingDevs_calendar-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &
 
 SERVICE_PID=$(pgrep -f $JAR)
 #echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
