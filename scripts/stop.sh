2d78fc783607eb26d37a27adc94ca84f30a013aa
kimsh1017 <81338287+kimsh1017@users.noreply.github.com>
2024-03-20 14:17:48 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,8 +2,11 @@ ROOT_PATH="/home/ubuntu/spring-github-action"
 JAR="$ROOT_PATH/application.jar"
 STOP_LOG="$ROOT_PATH/stop.log"
 SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
+SERVICE_JAVA_PID=$(pgrep java)
 
 if [ -n "$SERVICE_PID" ]; then
   kill "$SERVICE_PID"
+if [ -n "$SERVICE_JAVA_PID" ]; then
+  kill -9 "$SERVICE_JAVA_PID"
   # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
 fi
