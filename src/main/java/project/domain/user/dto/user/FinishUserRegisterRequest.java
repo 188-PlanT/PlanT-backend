e302d6388b898262aeb90981f80af2e12dbfdab0
seonghun <kimsh8580@gmail.com>
2024-04-24 16:28:02 +0900

    [BUG]: 회원가입 완료 로직 Validation 추가
    회원가입 완료 로직에 validation 로직이 빠져 nickName 없이 요청해도 USER_NOT_FOUND 에러가 나던 버그 수정
    
    [#?]

--- src/main/java/project/domain/user/dto/user/FinishUserRegisterRequest.java
+++ src/main/java/project/domain/user/dto/user/FinishUserRegisterRequest.java
@@ -2,7 +2,11 @@ package project.domain.user.dto.user;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 public class FinishUserRegisterRequest{
+
+    @NotBlank
     private String nickName;
 }
