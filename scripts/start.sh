6f8a36bfdf2289bf5efd4da95a2a865c07f750c8
kimsh1017 <81338287+kimsh1017@users.noreply.github.com>
2024-05-18 17:11:32 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-
 ROOT_PATH="/home/ubuntu/spring-github-action"
-JAR="$ROOT_PATH/application.jar"
-
-APP_LOG="$ROOT_PATH/application.log"
-ERROR_LOG="$ROOT_PATH/error.log"
-START_LOG="$ROOT_PATH/start.log"
-
 JAR=blazingDevs_calendar-0.0.1-SNAPSHOT.jar
-NOW=$(date +%c)
 
 export spring_profiles_active=prod
 nohup java -jar $ROOT_PATH/build/libs/$JAR > /dev/null 2> /dev/null < /dev/null &
-
-SERVICE_PID=$(pgrep -f java)
-#echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
