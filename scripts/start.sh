51b21057bb5ba25019a3b223469a3abfd343a3f4
kimsh1017 <81338287+kimsh1017@users.noreply.github.com>
2024-05-18 17:52:11 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,4 +5,4 @@ JAR=blazingDevs_calendar-0.0.1-SNAPSHOT.jar
 export spring_profiles_active=prod
 nohup java -jar $ROOT_PATH/build/libs/$JAR > /dev/null 2> /dev/null < /dev/null &
 
-echo -e "\n stop 실행" >> test.txt
+echo -e "\n start 실행" >> test.txt
