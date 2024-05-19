dfe0f5dd2ae592357b015834a52ee1850b24a3fb
kimsh1017 <81338287+kimsh1017@users.noreply.github.com>
2024-05-19 13:18:26 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,12 +1,10 @@
 #! /bin/bash
 
 ROOT_PATH="/home/ubuntu/spring-github-action"
-STOP_LOG="$ROOT_PATH/stop.log"
 SERVICE_JAVA_PID=$(pgrep java)
 
 if [ -n "$SERVICE_JAVA_PID" ]; then
   sudo kill -9 "$SERVICE_JAVA_PID"
-  # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
 fi
 
 echo -e "\n stop 실행" >> test.txt
