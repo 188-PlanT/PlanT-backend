183e5d9b93e63d9ae0ea845a5d92429ff54df49d
seonghun <kimsh8580@gmail.com>
2024-12-22 22:01:20 +0900

    [TEST]: 유저 도메인 Unit Test 생성
    유저 객체 생성 테스트 추가
    
    [#?]

--- src/test/java/project/domain/UserTest.java
+++ src/test/java/project/domain/UserTest.java
@@ -1,10 +1,21 @@
-// package project.domain;
+ package project.domain;
 
-// import org.junit.jupiter.api.*;
+ import org.junit.jupiter.api.*;
+ import project.domain.user.domain.User;
+ import project.domain.user.domain.UserRole;
 
-// @Tag("unit")
-// public class UserTest{
-    
-//     @Test
-    
-// }
+ public class UserTest{
+     @Test
+     @DisplayName("유저 객체 생성 테스트")
+     void createUser() {
+         User user = User.builder()
+                 .email("test1@gmail.com")
+                 .nickName("test1")
+                 .password("1234")
+                 .profile(null)
+                 .userRole(UserRole.USER)
+                 .build();
+
+         Assertions.assertEquals("test1@gmail.com", user.getEmail());
+     }
+ }
