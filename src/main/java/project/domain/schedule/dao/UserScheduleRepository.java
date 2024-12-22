c39d08b6e201b02d2b158a502be5ee2d54702c7b
seonghun <kimsh8580@gmail.com>
2024-12-22 22:00:36 +0900

    [FIX]: 유저 스케줄 날짜별 조회 로직 변경
    시작 일자와 종료 일자가 조회 범위를 벗어나는 경우, 조회가 되지 않던 버그 수정
    ex: 20240101 시작, 20241231 종료 스케줄을 date=202402로 조회
    
    [#?]

--- src/main/java/project/domain/schedule/dao/UserScheduleRepository.java
+++ src/main/java/project/domain/schedule/dao/UserScheduleRepository.java
@@ -12,6 +12,6 @@ public interface UserScheduleRepository extends JpaRepository<UserSchedule, Long
     @Query("select us from UserSchedule us " + 
 			"join fetch us.user u join fetch us.schedule s join fetch s.workspace w " +
 			"where us.user.email =:email and " +
-            "((us.schedule.startDate between :startDate and :endDate) or (us.schedule.endDate between :startDate and :endDate))")
+            "((us.schedule.startDate < :startDate and us.schedule.endDate >= :startDate) or (us.schedule.startDate between :startDate and :endDate))")
     public List<UserSchedule> searchByUser(String email, LocalDateTime startDate, LocalDateTime endDate);
 }
