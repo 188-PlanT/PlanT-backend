1587d5d5079bad654646845f421bb99bd84e1c36
kimsh1017 <kimsh8580@gmail.com>
2025-02-16 11:07:56 +0900

    [REFACTOR]: `@Query`에 value 추가
    본문
    
    [#?]

--- src/main/java/project/domain/schedule/dao/ScheduleRepository.java
+++ src/main/java/project/domain/schedule/dao/ScheduleRepository.java
@@ -17,16 +17,16 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long>, Sched
           countQuery = "select count(s) from Schedule s")
     public Page<Schedule> findAll(Pageable pageable);
     
-    @Query("select s from Schedule s " +
+    @Query(value = "select s from Schedule s " +
             "where s.workspace = :workspace " +
             "and ((s.startDate between :startDate and :endDate) or (s.endDate between :startDate and :endDate))")
     public List<Schedule> searchByMonth(Workspace workspace, LocalDateTime startDate, LocalDateTime endDate);
 
-    @Query("select s from Schedule s " +
+    @Query(value = "select s from Schedule s " +
             "where s.workspace = :workspace " +
             "and ((:startDate between s.startDate and s.endDate) or (:endDate between s.startDate and s.endDate))")
     public List<Schedule> searchByDate(Workspace workspace, LocalDateTime startDate, LocalDateTime endDate);
 
-    @Query("select s from Schedule s join fetch s.workspace w where s.id = :id")
+    @Query(value = "select s from Schedule s join fetch s.workspace w where s.id = :id")
 	public Optional<Schedule> findById(Long id);
 }
--- src/main/java/project/domain/schedule/dao/ScheduleRepositoryCustomImpl.java
+++ src/main/java/project/domain/schedule/dao/ScheduleRepositoryCustomImpl.java
@@ -3,12 +3,12 @@ package project.domain.schedule.dao;
 import java.util.List;
 
 import lombok.RequiredArgsConstructor;
-import static project.domain.schedule.domain.QSchedule.schedule;
-import static project.domain.schedule.domain.QUserSchedule.userSchedule;
-import static project.domain.workspace.domain.QWorkspace.workspace;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import project.domain.schedule.domain.Schedule;
+import static project.domain.schedule.domain.QSchedule.schedule;
+import static project.domain.schedule.domain.QUserSchedule.userSchedule;
+import static project.domain.workspace.domain.QWorkspace.workspace;
 
 
 //이건 그냥 전용 리포지토리 만드는게 좋아보임
