2fb7ba94ec08ff2758991333e33f8beb3438d3ac
kimsh1017 <kimsh8580@gmail.com>
2025-06-06 15:47:46 +0900

    [REFACTOR]: 유저 도메인 테스트 추가

---
+++ src/test/java/project/common/constant/UserConstant.java
@@ -0,0 +1,8 @@
+package project.common.constant;
+
+public class UserConstant {
+
+    public static final String EMAIL = "test1@gmail.com";
+    public static final String NICKNAME = "testUser";
+    public static final String PASSWORD = "testPassword";
+}
---
+++ src/test/java/project/domain/user/domain/UserTest.java
@@ -0,0 +1,41 @@
+package project.domain.user.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static project.common.constant.UrlConstant.*;
+import static project.common.constant.UserConstant.*;
+
+import org.junit.jupiter.api.Test;
+import project.domain.image.domain.Image;
+
+class UserTest {
+
+    @Test
+    void 유저_생성시_상태는_PENDING이다() {
+        // given
+        Image profileImage = new Image(DEFAULT_USER_PROFILE_URL);
+
+        // when
+        User emailPasswordUser = User.ofEmailPassword(EMAIL, PASSWORD, profileImage);
+        User oauthUser = User.fromOAuth2Attributes(EMAIL, profileImage);
+
+        // then
+        assertThat(emailPasswordUser.getUserRole()).isEqualTo(UserRole.PENDING);
+        assertThat(oauthUser.getUserRole()).isEqualTo(UserRole.PENDING);
+    }
+
+    @Test
+    void 닉네임_설정시_회원가입이_완료된다() {
+        // given
+        Image profileImage = new Image(DEFAULT_USER_PROFILE_URL);
+        User emailPasswordUser = User.ofEmailPassword(EMAIL, PASSWORD, profileImage);
+        User oauthUser = User.fromOAuth2Attributes(EMAIL, profileImage);
+
+        // when
+        emailPasswordUser.setNickName(NICKNAME);
+        oauthUser.setNickName(NICKNAME);
+
+        // then
+        assertThat(emailPasswordUser.getUserRole()).isEqualTo(UserRole.USER);
+        assertThat(oauthUser.getUserRole()).isEqualTo(UserRole.USER);
+    }
+}
