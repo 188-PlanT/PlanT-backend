c92c8c6f96fc4094015cd6997968c079dd302928
kimsh1017 <kimsh8580@gmail.com>
2025-09-21 15:43:14 +0900

    [REFACTOR]: ImageService 리팩토링

--- src/main/java/project/domain/image/service/ImageService.java
+++ src/main/java/project/domain/image/service/ImageService.java
@@ -2,22 +2,27 @@ package project.domain.image.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import project.common.constant.UrlConstant;
 import project.common.exception.ErrorCode;
 import project.common.exception.PlantException;
 import project.domain.image.dao.ImageRepository;
 import project.domain.image.domain.Image;
+import project.domain.image.dto.response.ImageUploadResponse;
 
 @Service
 @RequiredArgsConstructor
 public class ImageService {
     private final ImageRepository imageRepository;
 
-    public void save(String imageUrl) {
+    @Transactional
+    public ImageUploadResponse saveImage(String imageUrl) {
         Image image = new Image(imageUrl);
         imageRepository.save(image);
+        return ImageUploadResponse.of(imageUrl);
     }
 
+    @Transactional(readOnly = true)
     public Image getDefaultUserProfile() {
         return imageRepository
                 .findByUrl(UrlConstant.DEFAULT_USER_PROFILE_URL)
