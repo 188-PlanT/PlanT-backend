095db29e81f32e6e24a8e40853de25bcc79a4b15
kimsh1017 <kimsh8580@gmail.com>
2025-09-21 15:37:56 +0900

    [REFACTOR]: 유저 서비스 리팩토링

--- src/main/java/project/domain/user/dto/response/EmailOrNicknameCheckResponse.java
+++
@@ -1,3 +0,0 @@
-package project.domain.user.dto.response;
-
-public record EmailOrNicknameCheckResponse(boolean available) {}
---
+++ src/main/java/project/domain/user/dto/response/NicknameCheckResponse.java
@@ -0,0 +1,8 @@
+package project.domain.user.dto.response;
+
+public record NicknameCheckResponse(boolean available) {
+
+    public static NicknameCheckResponse of(boolean available) {
+        return new NicknameCheckResponse(available);
+    }
+}
