efa5d829a79c21335c5b676026987a16164671b3
kimsh1017 <kimsh8580@gmail.com>
2025-09-21 17:45:30 +0900

    [REFACTOR]: ScheduleUser API 엔드포인트 변경

--- src/main/java/project/domain/scheduleUser/api/ScheduleUserController.java
+++ src/main/java/project/domain/scheduleUser/api/ScheduleUserController.java
@@ -18,14 +18,14 @@ public class ScheduleUserController {
     private final ScheduleUserService scheduleUserService;
 
     @Operation(summary = "스케줄 유저 추가", description = "스케줄에 유저를 추가합니다.")
-    @PostMapping("/v1/schedules/users")
+    @PostMapping("/v1/schedule-users")
     public ResponseEntity<Long> addUserToSchedule(@RequestBody @Valid ScheduleUserCreateRequest request) {
         var response = scheduleUserService.addUserToSchedule(request.scheduleId(), request.userId());
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @Operation(summary = "스케줄 유저 제거", description = "스케줄에서 유저를 제거합니다.")
-    @DeleteMapping("/v1/schedules/users/{scheduleUserId}")
+    @DeleteMapping("/v1/schedule-users/{scheduleUserId}")
     public ResponseEntity<Void> removeUserToSchedule(@PathVariable Long scheduleUserId) {
         scheduleUserService.removeUserToSchedule(scheduleUserId);
         return ResponseEntity.ok().build();
