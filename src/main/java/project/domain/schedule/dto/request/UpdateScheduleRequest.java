cd525dcbaafb92396057007ac3ec85849f4dc956
kimsh1017 <kimsh8580@gmail.com>
2025-09-28 19:09:45 +0900

    [REFACTOR]: 스케줄 생성, 수정 dto에서 user 제거

--- src/main/java/project/domain/schedule/dto/request/UpdateScheduleRequest.java
+++ src/main/java/project/domain/schedule/dto/request/UpdateScheduleRequest.java
@@ -3,12 +3,10 @@ package project.domain.schedule.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import java.util.List;
 import project.domain.schedule.domain.Progress;
 
 public record UpdateScheduleRequest(
         @NotBlank String name,
-        @NotNull List<Long> users,
         @NotNull LocalDateTime startDate,
         @NotNull LocalDateTime endDate,
         @NotNull Progress state,
