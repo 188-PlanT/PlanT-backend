ca0a5f650a14a00e8352698529d7bd5c3f82c86f
kimsh1017 <kimsh8580@gmail.com>
2025-09-28 19:11:52 +0900

    [REFACTOR]: 미사용 리포지토리 제거

--- src/main/java/project/domain/schedule/dao/ScheduleRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package project.domain.schedule.dao;
-
-import java.util.List;
-import project.domain.schedule.domain.Schedule;
-
-public interface ScheduleRepositoryCustom {
-
-    public List<Schedule> searchSchedulesByUserEmail(String email);
-}
