c08279b34ac018b861cbafd1f0780f20066c6c30
kimsh1017 <kimsh8580@gmail.com>
2025-10-05 15:13:07 +0900

    [REFACTOR]: 응답 dto 메서드명 변경

--- src/main/java/project/domain/user/dto/response/UserWorkspacesResponse.java
+++ src/main/java/project/domain/user/dto/response/UserWorkspacesResponse.java
@@ -3,15 +3,14 @@ package project.domain.user.dto.response;
 import static java.util.stream.Collectors.toList;
 
 import java.util.List;
-import project.domain.user.domain.User;
 import project.domain.workspaceUser.domain.WorkspaceUser;
 import project.domain.workspaceUser.dto.WorkspaceUserDto;
 
 public record UserWorkspacesResponse(Long userId, List<WorkspaceUserDto> workspaceUsers) {
 
-    public static UserWorkspacesResponse from(User user, List<WorkspaceUser> workspaceUsers) {
+    public static UserWorkspacesResponse from(Long userId, List<WorkspaceUser> workspaceUsers) {
         List<WorkspaceUserDto> workspaceDtos =
                 workspaceUsers.stream().map(WorkspaceUserDto::of).collect(toList());
-        return new UserWorkspacesResponse(user.getId(), workspaceDtos);
+        return new UserWorkspacesResponse(userId, workspaceDtos);
     }
 }
